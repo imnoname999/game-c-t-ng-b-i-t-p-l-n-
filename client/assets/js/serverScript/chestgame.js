39018aebc5e2b92a0a43fc3675a3f97f55f55ebb
imnonamw999 <qpeffff@gmail.com>
2025-10-31 12:21:07 +0700

    cau truc file

---
+++ client/assets/js/serverScript/chestgame.js
@@ -0,0 +1,20 @@
+const ROWS = 9;
+const COLS = 9;
+
+function chestgame() {
+    let self ={};
+    self.start = function () { 
+
+    }
+  self.pause = function () {
+
+    }
+  self.end = function () { 
+
+    }
+
+    function  init () {
+        
+    }init( );
+    return self;
+}
